32d0fd6e5b0af87575600cee7e3636dd285edec1
Mahdi2105 <mahdih2105@gmail.com>
2021-11-01 11:59:29 +0000

    Added menu and items

--- index.js
+++ index.js
@@ -57,13 +57,13 @@ const {
   app.put('/api/restaurants/:id', async (req, res) => {
     try {
       // create a row in the database using sequelize create method
-      const update_ID = await Restaurant.update(req.body, {
+      const updateR_ID = await Restaurant.update(req.body, {
         where: {
           id: req.params.id
         }
       });
       // 200 = success
-      res.status(200).send(update_ID);
+      res.status(200).send(updateR_ID);
     } catch (e) {
       res.status(400).send(e.message);
     }
@@ -72,13 +72,13 @@ const {
   app.delete('/api/restaurants/:id', async (req, res) => {
     try {
       // create a row in the database using sequelize create method
-      const delete_ID = await Restaurant.destroy({
+      const deleteR_ID = await Restaurant.destroy({
         where: {
           id: req.params.id
         }
       });
       // 200 = success
-      res.status(200).send(delete_ID);
+      res.status(200).send(deleteR_ID);
     } catch (e) {
       res.status(400).send(e.message);
     }
@@ -91,6 +91,147 @@ const {
   // 3. create a suite of menu and menu item routes that will CRUD each resource
   
   // 4. find a way to relate the menu items to the menu and the menu to the restaurant
+
+  app.get('/api/menus', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menus = await Menu.findAll({});
+      
+      // 200 = success
+      res.status(200).send(menus);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.get('/api/menus/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menu_ID = await Menu.findOne({
+        where: {
+          id: req.params.id
+        }
+      });
+      
+      // 200 = success
+      res.status(200).send(rest_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.post('/api/menus', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menu = await Menu.create(req.body);
+  
+      // 201 = created a resource
+      res.status(201).send(menu);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.put('/api/menus/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const updateM_ID = await Menu.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+      // 200 = success
+      res.status(200).send(updateM_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.delete('/api/menus/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const deleteM_ID = await Menu.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      // 200 = success
+      res.status(200).send(deleteM_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+
+  app.get('/api/menuItems', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menuItems = await MenuItem.findAll({});
+      
+      // 200 = success
+      res.status(200).send(menuItems);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.get('/api/menuItems/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menuItem_ID = await Restaurant.findOne({
+        where: {
+          id: req.params.id
+        }
+      });
+      
+      // 200 = success
+      res.status(200).send(menuItem_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.post('/api/menuItems', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const menuItem = await MenuItem.create(req.body);
+  
+      // 201 = created a resource
+      res.status(201).send(menuItem);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.put('/api/menuItems/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const updateMI_ID = await MenuItem.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+      // 200 = success
+      res.status(200).send(updateMI_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
+
+  app.delete('/api/menuItems/:id', async (req, res) => {
+    try {
+      // create a row in the database using sequelize create method
+      const deleteMI_ID = await MenuItem.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      // 200 = success
+      res.status(200).send(deleteMI_ID);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  });
   
   /**
    * Synchronize all models with db
